35e6333c2361019e69f3e0333bec6de1afedeced
Hiren Kadam <hiren.kadam@logicalwings.com>
2020-07-28 18:03:07 +0530

    Initial Commit

---
+++ CircularProgressViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircularProgressViewDemo
+//
+//  Created by Hiren Kadam on 28/07/20.
+//  Copyright © 2020 Hiren Kadam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
