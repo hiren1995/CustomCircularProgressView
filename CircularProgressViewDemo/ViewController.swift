5cc25dbaf597f84994fab9855003f2eb4d1aa196
Hiren Kadam <hiren.kadam@logicalwings.com>
2020-07-29 11:00:36 +0530

    Custom CircularView

--- CircularProgressViewDemo/ViewController.swift
+++ CircularProgressViewDemo/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circularProgressView: CircularProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        /*
+         //MARK: If you want to make view programatically without storyboard
+         
+        let cp = CircularProgressView(frame: CGRect(x: 50.0, y: 50.0, width: 100.0, height: 100.0))
+        cp.trackColor = UIColor.red
+        cp.progressColor = UIColor.green
+        self.view.addSubview(cp)
+        */
+        
+        circularProgressView.progressColor = UIColor.green
+        circularProgressView.trackColor = UIColor.red
+        circularProgressView.targetValue = 100
+        circularProgressView.achievedValue = 50
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //MARK: call the setUI method of Circular view i view did Appear because once the view appears we get the final frame of that circular view to calculate the arc center for circular layer
+        circularProgressView.setUI()
+        
+    }
 }
 
